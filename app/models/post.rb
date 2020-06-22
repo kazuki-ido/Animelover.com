dadbe08df9795ad0f4d2017a69f1f126caf09f91
kazuki ido <0sn58m19324338p@gmail.com>
2020-06-22 12:39:32 +0000

    画像アップロード機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  mount_uploader :picture, PictureUploader
   validates :title, presence: true, length: { minimum: 5 } 
   validates :content, presence: true
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_002244) do
+ActiveRecord::Schema.define(version: 2020_06_22_100505) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "commenter"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_002244) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
