eb2d64146aed9fc0f236df71acb9349c1f460208
kazuki ido <0sn58m19324338p@gmail.com>
2020-07-13 05:42:37 +0000

    マイページの作成（修正１）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  validates :name, presence: true
+  validates :profile, length: { maximum: 200 }
   
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
   
   resources :relationships, only: [:create, :destroy]
-  resources :users, :only => [:show]
-  
+  resources :users, :only => [:show, :edit, :update]
+  get '/mypage' => 'users#mypage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
