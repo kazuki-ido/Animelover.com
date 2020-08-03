c5124aa6df39604ef850fc8e7a2773c03fa77673
kazuki ido <0sn58m19324338p@gmail.com>
2020-08-03 12:53:39 +0000

    routesの設定を整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  resources :posts do
-    resources :comments
-  end
   root to: 'home#index'
   get 'home/show'
   devise_for :users
-  
-  resources :users, only: [:index, :show]
+
+  resources :users, only: [:index, :show, :edit, :update]
   resources :posts, only: [:index, :show, :create] do
     resources :likes, only: [:create, :destroy]
+    resources :comments
   end
-  
+
   resources :relationships, only: [:create, :destroy]
-  resources :users, :only => [:show, :edit, :update]
   get '/mypage' => 'users#mypage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
