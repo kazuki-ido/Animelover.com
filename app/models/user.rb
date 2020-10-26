02f11dfcd360649f5485fc24653eb6aa332ff0ba
kazuki ido <0sn58m19324338p@gmail.com>
2020-10-26 10:00:56 +0000

    has_manyの定義箇所をまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,15 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-  def already_liked?(post)
-    self.likes.exists?(post_id: post.id)
-  end
-  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverse_of_relationships, source: :user
   
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
+
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
